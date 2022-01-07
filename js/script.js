ed0f4080c49a0f5e4652a6980f86352c9a190050
Deshawn Nelson <Deshawn.a.nelson@gmail.com>
2022-01-07 06:48:26 -0500

    fix: change fetch request from http to https

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ async function requestWeather(location) {
     const API_KEY = '4e955b4f015e6e1c3abe0c6a45555f95';
     try {
         const units = 'imperial';
-        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}&units=${units}`);
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}&units=${units}`);
         if (!response.ok) {
             throw new Error("Location not found, please try again.");
         }
